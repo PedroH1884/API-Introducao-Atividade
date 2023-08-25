d6c8365a15b2f12d60d9fa58e251cb1fd7ca7255
Pedro Henrique Mendes de Castro <132668885+PedroH1884@users.noreply.github.com>
2023-08-25 12:00:43 +0000

    Reorganizando Arquivos

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import roteadorUsuario from "./routes/usuario.js";
+import dotenv from "dotenv";
+import express from "express";
+dotenv.config();
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.json({
+    message: "API para CRUD usuario: link_gitgub",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
+
+app.use(roteadorUsuario);
